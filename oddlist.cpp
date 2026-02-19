d0e7351cb25a74e82cfea1d9298ceb0eea824b2e
anythiinq <108547358+anythiinq@users.noreply.github.com>
2026-02-18 17:40:45 -0800

    Handle empty list case in removeOdd
    Add check for empty list in removeOdd function

--- oddlist.cpp
+++ oddlist.cpp
@@ -1,5 +1,9 @@
 void removeOdd(list<int>& li)
 {
+    if (li.begin() == li.end()) {        // list is empty
+        return;
+    }
+    
     list<int>::iterator q = li.begin();
     while (q != li.end()) {
         if (*q % 2 == 1) {

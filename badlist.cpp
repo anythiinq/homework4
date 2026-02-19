697f97385c037c9c9477acda2d516907827b5e6e
anythiinq <108547358+anythiinq@users.noreply.github.com>
2026-02-19 14:30:33 -0800

    Implement removeBad function to clean low-rated movies

---
+++ badlist.cpp
@@ -0,0 +1,13 @@
+void removeBad(list<Movie*>& li)
+{
+    list<Movie*>::iterator q = li.begin();
+    while (q != li.end()) {
+        if ((*q)->rating() < 50) {
+            Movie* toBeErased = *q;
+            q = li.erase(q);
+            delete toBeErased;
+        }
+            
+        ++q;
+    }
+}
